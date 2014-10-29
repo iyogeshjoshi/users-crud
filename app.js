ed6eb695707b0beb4463c90061b8da92a34605e0
iyogeshjoshi <yogesh.bholu@gmail.com>
2014-10-29 12:44:51 +0530

    Basic session structure done

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var session = require('express-session');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -19,17 +21,30 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser())
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+//app.use(passport.initialize());
+//app.use(passport.session()); // persistent login sessions
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+// app.use('/', routes);
+// app.use('/users', users);
+app.use('/', function(req, res){
+    var sess = req.session;
+    if(sess.count){
+        sess.count++;
+    }else{
+        sess.count = 1;
+    }
+
+    res.send('Cont: '+sess.count);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+     var err = new Error('Not Found');
+     err.status = 404;
+     next(err);
 });
 
 // error handlers
